e8cf3c9c599a76261116563b58fd741ae19534e4
Steven Freeman <sfreeman422@gmail.com>
2016-11-22 21:03:46 -0500

    Added comments to the functions within the seatgeek controller to help clarify what each controller does.

--- controllers/seatgeekControl.js
+++ controllers/seatgeekControl.js
@@ -4,49 +4,49 @@ var router = express.Router();
 
 var request = require('request');
 
+
+//Route upon hitting hte seatgeek route. 
 router.get('/', function(req, res){
+
+//This function delcaration is to grab the ID for the artist by searching by artist name. 
+function getID(queryURL){
+		request(queryURL, function(err, resp, body){
+			if(!err && resp.statusCode == 200){
+				console.log(body.performers[0].id);
+				//console.log("Artist ID for "+body.performers.name+" is :"+fullBody.id);
+			}
+			else{
+				console.log(err);
+			}
+		})
+}
+
+
+//This function is to query the shows based on artist ID. This is not yet using artistID, instead it is using artistName and is not fully functional. 
+//This is where it all starts. We grab the data here, pass it to the getID function and then return the full URL to beb used by makeRequest. 
 function getInfo(){
-	//BEGIN DATA PULL FROM DATABASE AND ADD TO QUERY STRING
-	//======================================================================================
-	var performers = ['Against-me', 'Bad-Religion', 'Grouplove'];
+	//This will eventually be data pulled from the users top 20 artists. 
+	var performers = ['against-me', 'bad-religion', 'grouplove'];
 	var performerQuery = ''; 
 
+	var clientID = 'NjIzMjUyMXwxNDc5NDI2Nzkz';
+	var clientSecret = 'zcNRKxkuP2Nej_z4gj1wMZPYU3fA9pAjtCuBZSOC';
+
 	for(var i = 0; i < performers.length; i++){
-		//Used to determine when we are at the end of the array. 
-		var arrLength = performers.length-1; 
-		//For loop to go through the array and add the artists to the performerQuery which we will
-		//Then feed to the API.
-		for(var j = 0; j < performers[i].length; j++){
-			//Sets up a new string that includes the string from the array. 
-			var artist = performers[i];
-			if(artist.charAt(j) == " "){
-				console.log("This string has a space.");
-			}
-		}
-		//Adds the performer and a plus if we are not at the end of the array. 
-		if(i != arrLength){
-			performerQuery += artist+',';
-		}
-		//Adds just the artist if we are at the end of the array. 
-		else{
-			performerQuery += artist; 
-		}
+		var artist = performers[i];
+		console.log("Artist being tested is: "+artist);
+		var queryURL = 'https://api.seatgeek.com/2/performers?slug='+artist+'&client_id='+clientID+'&client_secret='+clientSecret
+		console.log("QueryURL is "+queryURL);
+		getID(queryURL);
 	}
 
 	return performerQuery; 
-	//END DATA PULL FROM DATABASE. QUERY STRING COMPLETE as PERFORMERQUERY
 }
 	
-	//=======================================================================================
-	//=======================================================================================
-	//=======================================================================================
-	
-	//BEGIN API CALL 
-	//=======================================================================================
-
+//This function declaration will be used to actually query the seatgeek api using all of our top artists by ID. Not yet functional using IDs. Relies on getInfo();
 function makeRequest(performerQuery){
 	var clientID = 'NjIzMjUyMXwxNDc5NDI2Nzkz';
-	var clientSecret = 'zcNRKxkuP2Nej_z4gj1wMZPYU3fA9pAjtCuBZSOC'
+	var clientSecret = 'zcNRKxkuP2Nej_z4gj1wMZPYU3fA9pAjtCuBZSOC';
 	console.log(performerQuery);
 	var queryURL = 'https://api.seatgeek.com/2/events?performers.id='+performerQuery+'&client_id='+clientID+'&client_secret='+clientSecret
 	request(queryURL, function(err, resp, body){
@@ -60,7 +60,8 @@ function makeRequest(performerQuery){
 	});
 }
 
-makeRequest(getInfo());
+getInfo();
+//makeRequest(getInfo());
 
 });
 
