9f02a0f56fe31b560f0678eee0b22f2de16b5380
Steven Freeman <sfreeman422@gmail.com>
2016-11-22 20:10:14 -0500

    Seatgeek API half working. Needs to be able to use the performers.id rather than performers.slug so that we can look up multiple performers at once as an OR statement rather than an and

--- controllers/seatgeekControl.js
+++ controllers/seatgeekControl.js
@@ -5,13 +5,12 @@ var router = express.Router();
 var request = require('request');
 
 router.get('/', function(req, res){
+function getInfo(){
 	//BEGIN DATA PULL FROM DATABASE AND ADD TO QUERY STRING
 	//======================================================================================
-
 	var performers = ['Against-me', 'Bad-Religion', 'Grouplove'];
 	var performerQuery = ''; 
-	var clientID = 'NjIzMjUyMXwxNDc5NDI2Nzkz';
-	var clientSecret = 'zcNRKxkuP2Nej_z4gj1wMZPYU3fA9pAjtCuBZSOC'
+
 	for(var i = 0; i < performers.length; i++){
 		//Used to determine when we are at the end of the array. 
 		var arrLength = performers.length-1; 
@@ -26,15 +25,17 @@ router.get('/', function(req, res){
 		}
 		//Adds the performer and a plus if we are not at the end of the array. 
 		if(i != arrLength){
-			performerQuery += artist+'&performers=';
+			performerQuery += artist+',';
 		}
 		//Adds just the artist if we are at the end of the array. 
 		else{
 			performerQuery += artist; 
 		}
-	}.then()
+	}
 
+	return performerQuery; 
 	//END DATA PULL FROM DATABASE. QUERY STRING COMPLETE as PERFORMERQUERY
+}
 	
 	//=======================================================================================
 	//=======================================================================================
@@ -43,18 +44,24 @@ router.get('/', function(req, res){
 	//BEGIN API CALL 
 	//=======================================================================================
 
-
-	request('https://api.seatgeek.com/2/events?performers.slug='+performerQuery+'&client_id='+clientID+'&client_secret='+clientSecret, function(err, resp, body){
+function makeRequest(performerQuery){
+	var clientID = 'NjIzMjUyMXwxNDc5NDI2Nzkz';
+	var clientSecret = 'zcNRKxkuP2Nej_z4gj1wMZPYU3fA9pAjtCuBZSOC'
+	console.log(performerQuery);
+	var queryURL = 'https://api.seatgeek.com/2/events?performers.id='+performerQuery+'&client_id='+clientID+'&client_secret='+clientSecret
+	request(queryURL, function(err, resp, body){
 		if (!err && resp.statusCode == 200) {
     		console.log(body)
   		}
   		else{
   			console.log(err);
+  			console.log(queryURL);
   		}
 	});
+}
 
+makeRequest(getInfo());
 
-console.log(performerQuery);
 });
 
 module.exports = router; 
